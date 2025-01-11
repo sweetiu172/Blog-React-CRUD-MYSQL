7797aa8ce533f2b57212f9af733c6faeba552b45
Shrk <tuan.lnm@cyberdino.io>
2025-01-11 20:33:29 +0700

    add: some minor changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
 const db = mysql.createConnection({
-host: "localhost",
-user: "root",
-password: "",
-database:"blog_posts" 
+    host: "localhost",
+    port: 3305,
+    user: "root",
+    password: "root",
+    database:"blog_posts" 
 })
 
 module.exports = db;
